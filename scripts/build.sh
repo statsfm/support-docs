58492ab38337b1a9e70b46a4f921c8ca181fb5e7
Stijn van der Kolk <stijn@kolkies.dev>
2022-02-10 00:07:41 +0100

    build: only publish when not preview build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 yarn write-translations
-yarn crowdin upload sources
-yarn crowdin download
+# If DEPLOY_CONTEXT is set to preview or not set, then we will not upload and download from crowdin
+if [ "$DEPLOY_CONTEXT" == "preview" ]; then
+  echo "Skipping crowdin upload and download"
+else
+  echo "Uploading and downloading translations from crowdin..."
+  yarn crowdin-upload
+  yarn crowdin-download
+fi
 yarn build
