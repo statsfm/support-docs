fea3ddc352caffb27d2949375179acf62864cf6b
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-03-24 02:10:07 +0100

    feat(import): added mail generator with spotify login

---
+++ src/components/DataRequestGenerator/FramedGenerator.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+export default class FramedGenerator extends Component<
+  {},
+  { iFrameHeight: string }
+> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      iFrameHeight: "100px",
+    };
+  }
+
+  render() {
+    return (
+      <iframe
+        style={{
+          width: "100%",
+          height: this.state.iFrameHeight,
+          overflow: "visible",
+        }}
+        onLoad={() => {
+          const obj = ReactDOM.findDOMNode(this);
+          setInterval(() => {
+            obj.contentWindow.postMessage("heightPls", "*");
+          }, 100);
+          window.addEventListener(
+            "message",
+            (event) => {
+              if ("height" in event.data) {
+                this.setState({
+                  iFrameHeight: event.data.height,
+                });
+              }
+            },
+            false
+          );
+        }}
+        ref="iframe"
+        src="https://spotify-endsong-mail-generator.pages.dev?framed=true"
+        width="100%"
+        allow="clipboard-read; clipboard-write"
+        height={this.state.iFrameHeight}
+        scrolling="no"
+        frameBorder="0"
+      />
+    );
+  }
+}
