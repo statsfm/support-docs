daf68216a7c9b5866b1a2f44002c382c657ce9e9
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-03-24 02:23:54 +0100

    feat: moved generator to same host

--- src/components/DataRequestGenerator/FramedGenerator.tsx
+++ src/components/DataRequestGenerator/FramedGenerator.tsx
@@ -38,7 +38,7 @@ export default class FramedGenerator extends Component<
           );
         }}
         ref="iframe"
-        src="https://spotify-endsong-mail-generator.pages.dev?framed=true"
+        src={`https://${location.host}/endsong_mail_generator.html?framed=true`}
         width="100%"
         allow="clipboard-read; clipboard-write"
         height={this.state.iFrameHeight}
