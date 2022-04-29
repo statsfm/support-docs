3098bb7097ce12b4f203dfd8f5f4c99987a4f50e
Stijn van der Kolk <stijn@kolkies.dev>
2022-04-29 20:06:35 +0200

    feat: redirect to docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,9 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import config from '@generated/docusaurus.config';
-import Translate from '@docusaurus/Translate';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <>
-      <header className={clsx('hero', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link className="button button--secondary button--lg" to="/docs">
-              <Translate description="The text on the button to go to the support documentation">
-                Go to the Support Docs
-              </Translate>
-            </Link>
-          </div>
-        </div>
-      </header>
-    </>
-  );
-}
+import React, { useEffect } from 'react';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description={config.tagline}
-    >
-      <HomepageHeader />
-    </Layout>
-  );
+  useEffect(() => {
+    window.location.replace('/docs');
+  }, []);
+
+  return <div />;
 }
