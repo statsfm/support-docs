d2a43c507f30b5961b1dab12bfe1865360bc641d
Stijn van der Kolk <stijn@kolkies.dev>
2022-10-17 23:57:40 +0200

    fix: link to correct page

--- src/components/ImportGuide/TurnOnSync.tsx
+++ src/components/ImportGuide/TurnOnSync.tsx
@@ -18,7 +18,7 @@ export default function StepThree(): JSX.Element {
         }}
       >
         {
-          "The files you receive from Spotify miss the last 2-5 days of data. For example: if you've requested the data on June 1st and you've received the data on June 14th, the data will include streams until about June 12th. You can turn on the setting _Sync streaming history_ after you've requested your files to make sure you don't miss any data. You can do so in Settings > Algorithms, import & stream sync >_Sync streaming history. Make sure to set this to Enabled. You can read more about streaming history synchronisation {supportLink}."
+          "The files you receive from Spotify miss the last 2-5 days of data. For example: if you've requested the data on June 1st and you've received the data on June 14th, the data will include streams until about June 12th. You can turn on the setting _Sync streaming history_ after you've requested your files to make sure you don't miss any data. You can do so in Home > Settings (cog icon in the top right) > Stats & algorithms > Sync streaming history. Make sure to set this to Enabled. You can read more about streaming history synchronisation {supportLink}."
         }
       </Translate>
     </p>
